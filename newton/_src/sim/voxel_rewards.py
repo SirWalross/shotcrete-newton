c28565192f91c7995181960e1cb36717d8ba8664
SirWalross <27106448+SirWalross@users.noreply.github.com>
2026-01-07 09:21:14 +0100

    Track previous values of rewards for voxel solver

--- newton/_src/sim/voxel_rewards.py
+++ newton/_src/sim/voxel_rewards.py
@@ -11,11 +11,17 @@ class VoxelRewards:
             self.distance = wp.zeros((size[0], size[1] // 16, size[2] // 16), dtype=wp.float32)
             self.smoothness = wp.zeros((size[0], size[1] // 16, size[2] // 16), dtype=wp.float32)
             self.air_gap = wp.zeros((size[0], size[1] // 16, size[2] // 16), dtype=wp.float32)
+            self.prev_distance = wp.zeros((size[0], size[1] // 16, size[2] // 16), dtype=wp.float32)
+            self.prev_smoothness = wp.zeros((size[0], size[1] // 16, size[2] // 16), dtype=wp.float32)
+            self.prev_air_gap = wp.zeros((size[0], size[1] // 16, size[2] // 16), dtype=wp.float32)
             self.adhesion_failure_amount = wp.zeros((size[0],), dtype=wp.float32)
 
-    def clear(self):
+    def step(self):
+        self.prev_distance, self.distance = self.distance, self.prev_distance
         self.distance.zero_()
+        self.prev_smoothness, self.smoothness = self.smoothness, self.prev_smoothness
         self.smoothness.zero_()
+        self.prev_air_gap, self.air_gap = self.air_gap, self.prev_air_gap
         self.air_gap.zero_()
         self.adhesion_failure_amount.zero_()
 
